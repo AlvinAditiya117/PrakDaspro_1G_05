979267b7c53ce896b1395516a14d43ecbf7496a9
AlvinAditiya117 <ryuzavin@gmail.com>
2023-09-15 14:05:01 +0700

    P3_percobaan1-05

---
+++ src/P3/ContohVariable05.java
@@ -0,0 +1,18 @@
+package P3;
+
+public class ContohVariable05 {
+    public static void main(String[] args) {
+    String salahsatuhobysayaadalah = "Bermain petak umpet";
+    boolean ispandai =  true;
+    char jeniskelamin = 'L';
+    byte _umursayasekarang = 20;
+    double $ipk = 3.42, tinggi = 1.87;
+    System.out.println(salahsatuhobysayaadalah);
+    System.out.println("apakah pandai? " + ispandai);
+    System.out.println("jenis kelamin: " + jeniskelamin);
+    System.out.println("umurku saat ini: " + _umursayasekarang);
+    System.out.println(String.format("saya beripk %s dengan tinggi badan %s", $ipk,tinggi));
+    
+}
+    
+}
