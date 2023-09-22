1d2a26f6d315b894e4288b4674d435ef330ad3e8
AlvinAditiya117 <ryuzavin@gmail.com>
2023-09-22 16:10:55 +0700

    P4_percobaan3_pertanyaan1_05

--- src/P3/P4/HaragaBayar05.java
+++ src/P3/P4/HaragaBayar05.java
@@ -8,11 +8,19 @@ public class HaragaBayar05 {
 
         int harga,jumlah,jmlHalBuku;
         double dis = 0.1, total, bayar, jmlDis;
+        String merek;
+
 
         System.out.println("masukkan harga yang dibeli");
         harga=sc05.nextInt();
         System.out.println("masukkan jumlah jumlah barang yang dibeli");
         jumlah=sc05.nextInt();
+        System.out.println("merek buku yang dibeli");
+        merek=sc05.nextLine();
+        System.out.println("jumlah halaman buku");
+        jmlHalBuku=sc05.nextInt();
+        System.out.println("besar diskon yang diperoleh");
+        dis=sc05.nextDouble();
 
         total=harga*jumlah;
         jmlDis=total*dis;
