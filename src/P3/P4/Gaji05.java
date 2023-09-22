6a9d8c55107992fbc1ba76f0b1abddfc6926f5b0
AlvinAditiya117 <ryuzavin@gmail.com>
2023-09-22 14:16:24 +0700

    P4_percobaan2_05

--- src/P3/P4/Gaji05.java
+++ src/P3/P4/Gaji05.java
@@ -6,5 +6,19 @@ public class Gaji05 {
     public static void main(String[] args) {
         Scanner sc05 = new Scanner(System.in);
 
+        int jmlMasuk, jmlTdkMasuk,totGaji;
+        int gaji = 40000, potGaji = 25000;
+
+        System.out.println("masukkan jumlah hari kerja anda");
+        jmlMasuk=sc05.nextInt();
+        System.out.println("masukkan jumlah hari tidak kerja anda");
+        jmlTdkMasuk=sc05.nextInt();
+        System.out.println("masukkan jumlah gaji");
+        gaji=sc05.nextInt();
+        System.out.println("masukkan jumlah potongan gaji");
+        potGaji=sc05.nextInt();
+
+        System.out.println("gaji yang anda terima adalah"+ potGaji);
+
     }
 }
