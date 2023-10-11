257432196cb0a0c7c0d21f8db8224177be78f226
AlvinAditiya117 <ryuzavin@gmail.com>
2023-10-11 23:31:31 +0700

    P6_permilihan2percobaan1_pertanyaan2

--- src/P3/P6/Pemilihan2Percobaan105.java
+++ src/P3/P6/Pemilihan2Percobaan105.java
@@ -8,7 +8,9 @@ public class Pemilihan2Percobaan105 {
         Scanner sc05 = new Scanner(System.in);
         System.out.print("Masukkan Tahun : ");
         tahun = sc05.nextInt();
-    if ((tahun % 4) == 0 ) {
+    if ((tahun % 400) == 0) 
+        System.out.println("Tahun Kabisat");
+    else if ((tahun % 4) == 0 ) {
     if ((tahun % 100) != 0)
         System.out.println("Tahun Kabisat");
     else 
