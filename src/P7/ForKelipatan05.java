9d77a14c3f462ad26a54c3c2dac1573942d66fe0
AlvinAditiya117 <ryuzavin@gmail.com>
2023-10-13 22:27:48 +0700

    percobaan1_pertanyaan3_05

--- src/P7/ForKelipatan05.java
+++ src/P7/ForKelipatan05.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class ForKelipatan05 {
     public static void main(String[] args) {
-        Scanner sc05 = new Scanner(System.in); 
-        int kelipatan, counter=0, total=0;
+        Scanner sc05 = new Scanner(System.in);        
+        int kelipatan, jumlah = 0, counter = 0, total=0;
+        double rata2=0;
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = sc05.nextInt();
         for (int i = 1; i <= 50; i++) {
@@ -14,9 +15,11 @@ public class ForKelipatan05 {
                 counter++;
     }
 }
+        if (counter != 0) {
+            rata2 = (double) jumlah / counter;
+}
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
-        System.out.println("total : " + total);
-        sc05.close();
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.println("Rata rata bilangan kelipatan " + kelipatan + " dari 1 sampai 50 adalah " + rata2);
     }
 }
