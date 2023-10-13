d56c39e7424dc6b43da82ca73c237f0518243a9f
AlvinAditiya117 <ryuzavin@gmail.com>
2023-10-13 22:26:13 +0700

    DoWhileCuti05

---
+++ src/P7/DoWhileCuti05.java
@@ -0,0 +1,29 @@
+package P7;
+
+import java.util.Scanner;
+
+public class DoWhileCuti05 {
+    public static void main(String[] args) {
+        Scanner sc05 = new Scanner(System.in);
+        int jc, jh;
+        String konfirmasi;
+        System.out.print("Jatah cuti: ");
+        jc = sc05.nextInt();
+    do {
+        System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+        konfirmasi = sc05.next();
+    if (konfirmasi.equalsIgnoreCase("y")) {
+        System.out.print("Jumlah hari: ");
+        jh = sc05.nextInt();       
+    if (jh <= jc) {
+        jc -= jh;
+        System.out.println("Sisa jatah cuti: " + jc);
+    } else {
+        System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+   break;
+    }
+}
+} while (jc > 0);
+    }
+}
+
