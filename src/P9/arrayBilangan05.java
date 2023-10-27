aee071bee1aee082d97821f4958e930791e5a4a2
AlvinAditiya117 <ryuzavin@gmail.com>
2023-10-27 13:18:21 +0700

    P9_percobaan1_05

---
+++ src/P9/arrayBilangan05.java
@@ -0,0 +1,18 @@
+package P9;
+
+import java.util.Scanner;
+
+public class arrayBilangan05 {
+    public static void main(String[]args){
+    Scanner sc05 = new Scanner(System.in);
+    int Bill[] = new int [4] ;
+    Bill [0] = 5 ;
+    Bill [1] = 13 ;
+    Bill [2] = -7 ;
+    Bill [3] = 17 ;
+    System.out.println(Bill[0]);
+    System.out.println(Bill[1]);
+    System.out.println(Bill[2]);
+    System.out.println(Bill[3]);
+}
+}
