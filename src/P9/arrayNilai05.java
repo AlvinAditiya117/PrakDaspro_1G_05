cd8ca2d39cdbe6985aaab908c6f64e0b45dc5561
AlvinAditiya117 <ryuzavin@gmail.com>
2023-10-27 13:20:39 +0700

    P9_percobaan2_05

---
+++ src/P9/arrayNilai05.java
@@ -0,0 +1,17 @@
+package P9;
+
+import java.util.Scanner;
+
+public class arrayNilai05 {
+    public static void main(String[]args){
+    Scanner sc05 = new Scanner(System.in);
+    int nilaiAkhir[] = new int [10] ;
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+        System.out.print("masukan nilai akhir ke-"+i+" :");
+        nilaiAkhir[i] = sc05.nextInt();
+    }
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+        System.out.print("masukan nilai akhir ke-"+i+"adalah "+nilaiAkhir[i]);  
+    }
+    }
+}
