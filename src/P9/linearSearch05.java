35efd3d0ac89b20cd64032d5210f81c6051c5ffc
AlvinAditiya117 <ryuzavin@gmail.com>
2023-11-03 21:36:42 +0700

    P9_percobaan4_pertanyaan2_05

--- src/P9/linearSearch05.java
+++ src/P9/linearSearch05.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class linearSearch05 {
     public static void main(String[]args){
     Scanner sc05 = new Scanner(System.in);
-    int arrayInt [] = {24,18,26,48,72,20,56,63};
-    int key = 20 ;
+    int n = 0;
+    int key ;
     int hasil = 0 ;
+    System.out.println("masukkan jumlah element array : ");
+    n = sc05.nextInt();
+    int[] arrayInt = new int[n];
 
     for (int i = 0; i < arrayInt.length; i++) {
+        System.out.println("masukkan element aray ke- "+i+" :");
+        arrayInt[i] = sc05.nextInt();
+    }
+    System.out.println("masukkan key yang ingin dicari : ");
+    key = sc05.nextInt();
+    
+        for (int i = 0; i < arrayInt.length; i++) {
         if (arrayInt[i] == key) {
             hasil = i ;
             break ;
