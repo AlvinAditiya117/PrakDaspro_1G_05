9f91e6cdae9c9ef99a66eef014cd6e63e36976ad
AlvinAditiya117 <ryuzavin@gmail.com>
2023-11-03 15:19:56 +0700

    P9_oercobaan2_pertanyaan4_05

--- src/P9/arrayNilai05.java
+++ src/P9/arrayNilai05.java
@@ -11,7 +11,11 @@ public class arrayNilai05 {
         nilaiAkhir[i] = sc05.nextInt();
     }
     for (int i = 0; i < nilaiAkhir.length; i++) {
-        System.out.println("masukan nilai akhir ke- "+i+" adalah "+nilaiAkhir[i]);  
+        if (nilaiAkhir[i] > 70) {
+            System.out.println("mahasiswa ke- "+i+" lulus "); 
+        } else {
+            System.out.println("mahasiswa ke- "+i+" tidak lulus "); 
+        }
     }
     }
 }
