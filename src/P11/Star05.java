ebb3b66ca79473e13b8be743c2f964aba1b5a289
AlvinAditiya117 <ryuzavin@gmail.com>
2023-11-16 18:22:34 +0700

    P11_percobaan1_05

---
+++ src/P11/Star05.java
@@ -0,0 +1,16 @@
+package P11;
+
+import java.util.Scanner;
+
+public class Star05 {
+    public static void main(String[] args) {
+        Scanner sc05 = new Scanner(System.in);
+
+        System.out.print("masukkan nilai N : ");
+        int N = sc05.nextInt();
+
+        for (int i = 1; i <=N; i++) {
+            System.out.print("*");
+        }
+    }
+}
