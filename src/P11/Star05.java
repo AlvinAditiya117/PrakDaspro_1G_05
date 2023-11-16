373140f64e4ee3e50b5eae9253ef90be270b99bd
AlvinAditiya117 <ryuzavin@gmail.com>
2023-11-16 18:41:18 +0700

    P11_percobaan1_pertanyaan3_05

--- src/P11/Star05.java
+++ src/P11/Star05.java
@@ -9,7 +9,7 @@ public class Star05 {
         System.out.print("masukkan nilai N : ");
         int N = sc05.nextInt();
 
-        for (int i = 0; i >=N; i++) {
+        for (int i = 1; i >=N; i--) {
             System.out.print("*");
         }
     }
