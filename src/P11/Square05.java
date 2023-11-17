7189f8c0aea4f5aef627e68193acf96a3d1f1885
AlvinAditiya117 <ryuzavin@gmail.com>
2023-11-17 15:38:18 +0700

    P11_percobaan2_pertanyaan2_05

--- src/P11/Square05.java
+++ src/P11/Square05.java
@@ -10,7 +10,7 @@ public class Square05 {
         int N = sc05.nextInt();
 
         for (int j = 1; j <+N ; j++) {
-            for (int i = 0; i <=N ; i++) {
+            for (int i = 1; i <=N ; i++) {
                 System.out.print("*");
             }
             System.out.println();
