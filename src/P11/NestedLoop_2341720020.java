46ad4265d4413a609d381408a892d7640cdc59b2
AlvinAditiya117 <ryuzavin@gmail.com>
2023-11-17 16:50:42 +0700

    P11_percobaan3_pertanyaan3_05

--- src/P11/NestedLoop_2341720020.java
+++ src/P11/NestedLoop_2341720020.java
@@ -7,20 +7,27 @@ public class NestedLoop_2341720020 {
         Scanner sc05 = new Scanner(System.in);
 
         double temps[][] = new double[5][7];
+        int i, j, counter, sum;
+        double rata2;
 
-        for (int i = 0; i < temps.length; i++) {
+        for ( i = 0; i < temps.length; i++) {
             System.out.println("kota ke-" + i);
-            for (int j = 0; j < temps[0].length; j++) {
+            for ( j = 0; j < temps[0].length; j++) {
                 System.out.print("hari ke-" + (j + 1) + " : ");
                 temps[i][j] = sc05.nextDouble();
             }
         }
 
-        for (double[] tempsVar : temps) {
-            for (double variable : tempsVar) {
-                System.out.print(variable+"");
+        for (i = 0; i < temps.length; i++) {
+            rata2 = 0;
+            System.out.println("kota ke-" + (i + 1) + "");
+            for (j = 0, counter = 0, sum = 0; j < temps[0].length; j++) {
+                System.out.print(temps[i][j] + "");
+                counter++;
+                sum += temps[i][j];
             }
-            System.out.println();
+            rata2 = (double) sum / counter;
+            System.out.println(" rata rata : " + rata2);
         }
     }
 }
