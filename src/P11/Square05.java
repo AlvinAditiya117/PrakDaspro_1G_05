9b31c7d57861234830ea9be042e38ce25626656a
AlvinAditiya117 <ryuzavin@gmail.com>
2023-11-17 15:20:36 +0700

    P11_percobaan2_05

---
+++ src/P11/Square05.java
@@ -0,0 +1,19 @@
+package P11;
+
+import java.util.Scanner;
+
+public class Square05 {
+    public static void main(String[] args) {
+        Scanner sc05 = new Scanner (System.in);
+
+        System.out.print("masukkan nilai N : ");
+        int N = sc05.nextInt();
+
+        for (int j = 1; j <+N ; j++) {
+            for (int i = 1; i <=N ; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
