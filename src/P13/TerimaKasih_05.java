2f1112e36ec53e1ea224243ce8e9697605213697
AlvinAditiya117 <ryuzavin@gmail.com>
2023-12-01 13:22:59 +0700

    P13_pecobaan2_05

--- src/P13/TerimaKasih_05.java
+++ src/P13/TerimaKasih_05.java
@@ -5,7 +5,12 @@ public static void UcapanTerimaKasih(){
     System.out.println("Thank you fpr being the best teacher in the world.\n"+
     "you inspired in me a love for learning and made me feel like i could ask you anything");
 }
-public static void main(String[]args){
+public static void UcapanTambahan(String ucapan) {
+    System.out.println(ucapan);
+}
+public static void main(String[] args) {
     UcapanTerimaKasih();
+    String ucapan = "Terima Kasih pak... Bu... Semoga Sehat Selalu";
+    UcapanTambahan(ucapan);
 }
 }
