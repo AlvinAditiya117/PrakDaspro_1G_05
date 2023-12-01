0d4ad4baaec4f7b98a0d6a646fc82b7d95d8cf58
AlvinAditiya117 <ryuzavin@gmail.com>
2023-12-01 19:44:26 +0700

    P13_percobaan4_pertanyaan3_05

--- src/P13/UcapanTerimaKasih_05.java
+++ src/P13/UcapanTerimaKasih_05.java
@@ -3,6 +3,9 @@ package P13;
 import java.util.Scanner;
 
 public class UcapanTerimaKasih_05 {
+   public static void UcapanTambahan(String tambahan) {
+      System.out.print(tambahan);
+   }
    public static String PenerimaUcapan(){
     Scanner sc05 = new Scanner(System.in);
     System.out.print("Tuliskan nama orang yang ingin anda beri ucapan : ");
@@ -13,8 +16,13 @@ public class UcapanTerimaKasih_05 {
    public static void UcapanTerimaKasih(){
     String nama = PenerimaUcapan();
     System.out.println("Thank you "+nama+"\nMay the force be with you");
+    UcapanTambahan("ini adalah ucapan tambahan");
    }
    public static void main(String[] args) {
+      Scanner sc05 = new Scanner(System.in);
+      System.out.print("Tuliskan ucapan tambahan ");
+      String UcapTambahan = sc05.nextLine();
     UcapanTerimaKasih();
+    UcapanTambahan(UcapTambahan);
    }
 }
