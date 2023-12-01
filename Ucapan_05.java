0bedb8175c832a52d930d85bf7016e92e97f7460
AlvinAditiya117 <ryuzavin@gmail.com>
2023-12-01 13:46:33 +0700

    P13_percobaan3_05

--- Ucapan_05.java
+++ Ucapan_05.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Ucapan_05 {
     public static String PenerimaUcapan() {
         Scanner sc05 = new Scanner(System.in);
-        System.out.println("Tulisakan nama orang yanng anda beri ucapan");
-        String namaOrang = sc.nextLine();
-        sc.close();
+        System.out.print("Tulisakan nama orang yanng anda beri ucapan : ");
+        String namaOrang = sc05.nextLine();
+        sc05.close();
         return namaOrang;
     }
     public static void main(String[] args) {
         String nama = PenerimaUcapan();
-        System.out.println("Thank you"+nama+"\nMay the force be with you");
+        System.out.println("Thank you "+nama+"\nMay the force be with you");
     }
 }
