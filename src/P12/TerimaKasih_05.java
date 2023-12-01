5ab43c3e06289ce1e4452b8ca161097d835a920c
AlvinAditiya117 <ryuzavin@gmail.com>
2023-12-01 13:07:35 +0700

    P12_pecobaan1_05

---
+++ src/P12/TerimaKasih_05.java
@@ -0,0 +1,12 @@
+package P12;
+
+public class TerimaKasih_05 {
+public static void UcapanTerimaKasih(){
+    System.out.println("Thank you fpr being the best teacher in the world.\n"+
+    "you inspired in me a love for learning and made me feel like i could ask you anything");
+}
+public static void main(String[]args){
+    UcapanTerimaKasih();
+
+}
+}
