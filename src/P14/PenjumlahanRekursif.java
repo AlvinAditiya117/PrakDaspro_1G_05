088a8dc8c3680b84f402f4e99ecc6b5a15c5cfa9
AlvinAditiya117 <ryuzavin@gmail.com>
2023-12-08 16:36:25 +0700

    P14_tugas2_perbaikan_05

--- src/P14/PenjumlahanRekursif.java
+++ src/P14/PenjumlahanRekursif.java
@@ -3,19 +3,19 @@ package P14;
 import java.util.Scanner;
 
 public class PenjumlahanRekursif {
-    public static void main(String[] args) {
-    Scanner sc05 = new Scanner(System.in);
-    System.out.print("masukkan nilai f : ");
-    int f = sc05.nextInt();
-    int hasil = jumlahkanBilangan(f);
-    System.out.print("menjumlahkan bilangan awal 1 sampai "+f+" adalah "+hasil);
-    sc05.close();
+    static int jumlahKanBilangan(int f, int start) {
+        System.out.print(start);
+        if (start >= f)
+            return start;
+        System.out.print(" + ");
+        return start + jumlahKanBilangan(f, start + 1);
     }
-    public static int jumlahkanBilangan(int n) {
-        if (n == 1) {
-            return 1;
-        }else {
-            return n + jumlahkanBilangan(n - 1);
-        }
+
+    public static void main(String[] args) {
+        Scanner sc05 = new Scanner(System.in);
+        System.out.print("Masukkan bilangan: ");
+        int f = sc05.nextInt();
+        System.out.println(" = " + jumlahKanBilangan(f, 1));
+        sc05.close();
     }
 }
