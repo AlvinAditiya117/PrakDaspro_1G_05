a82be516169573927d73b47aa08208a6c6d44e1c
AlvinAditiya117 <ryuzavin@gmail.com>
2023-12-08 15:57:13 +0700

    P14_tugas4_05

---
+++ src/P14/fibonacci.java
@@ -0,0 +1,23 @@
+package P14;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        int bulan = 12;
+        System.out.println("Bulan ke\t| Produktif\t| Belum Produktif\t| Jumlah Pasangan");
+        System.out.println("=========================================================================");
+        for (int i = 1; i <= bulan; i++) {
+            int totalPasangan = hitungFibonacci(i);
+            int pasanganBelumProduktif = hitungFibonacci(i - 1);
+            int pasanganProduktif = totalPasangan - pasanganBelumProduktif;
+
+            System.out.printf("%-10d\t| %-18d\t| %-12d\t| %-16d%n", i, pasanganProduktif, pasanganBelumProduktif, totalPasangan);
+        }
+    }
+    public static int hitungFibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return hitungFibonacci(n - 1) + hitungFibonacci(n - 2);
+        }
+    }
+}
